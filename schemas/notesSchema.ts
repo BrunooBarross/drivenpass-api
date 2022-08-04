bd785fc207e617af0c5dbf9cf17e20b2d59adb47
Rodrigodcs <rodrigodcsee@gmail.com>
2022-08-04 14:54:23 -0300

    Preparando revisão de código

--- schemas/notesSchema.ts
+++
@@ -1,9 +0,0 @@
-import joi from "joi";
-import { NoteInsertData } from "../repositories/notesRepository.js";
-
-const notesSchema = joi.object<NoteInsertData>({
-    title: joi.string().max(50).required(),
-    note: joi.string().max(1000).required()
-});
-
-export { notesSchema };
